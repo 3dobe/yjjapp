95732f941df473c56042f5160113fadb915d4d26
node200ok <node200ok@163.com>
2013-10-06 19:00:44 +0800

    config

--- config/local.js
+++ config/local.js
@@ -1,15 +1,5 @@
 
-var rootDir = __dirname + '/..',
-    publicDir = rootDir + '/public',
-    shareDir = publicDir + '/files';
-
 module.exports = {
 	env: 'production',
-	port: 3088,
-	timezoneOffset: 8,
-	secret: '' + Math.random(),
-	rootDir: rootDir,
-	tmpDir: rootDir,
-	publicDir: publicDir,
-    shareDir: shareDir
+	port: 3088
 }
