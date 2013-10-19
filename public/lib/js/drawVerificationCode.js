3b96e1905d12ef1377344226f6cf08acd0cf71aa
fritzlium <fritzlium@163.com>
2013-10-19 16:23:47 +0800

    better font

--- public/lib/js/drawVerificationCode.js
+++ public/lib/js/drawVerificationCode.js
@@ -2,7 +2,7 @@ var ctx;
 
 function drawVerificationCode(firstArg,secondArg,operator){
     ctx = document.getElementById('canvas').getContext('2d');
-    ctx.font = "15px Handwriting";
+    ctx.font = "15px Arial";
 
     ctx.background = "rgb(0.255.255)";
     ctx.clearRect(0, 0, 70, 18);
