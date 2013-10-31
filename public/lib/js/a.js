2c24a5ed06b241de3f41d5a9448422b91b7203b7
node200ok <node200ok@163.com>
2013-10-31 22:51:50 +0800

    /do/s/me /do/a/me

--- public/lib/js/a.js
+++ public/lib/js/a.js
@@ -1,16 +1,13 @@
 
 clientType = 'a';   // 客户端类型
 
-// 本地存储
-store.get(clientType) || store.set(clientType, {
-    talkID: ''
+$.get('/do/a/me', function(resObj) {
+    var lectid = resObj['lectid'];
+    if (lectid) { // 已有讲座
+        hashPage = location.hash || '#/view';
+    } else {
+        hashPage = location.hash || '#/join';
+    }
+    // 加载子页面
+    loadFrame(hashPage);
 });
-var storeS = store.get(clientType);
-
-if (storeS['talkID']) { // 已有讲座
-    hashPage = location.hash || '#/view';
-} else {
-    hashPage = location.hash || '#/join';
-}
-// 加载子页面
-loadFrame(hashPage);
--- public/lib/js/s.js
+++ public/lib/js/s.js
@@ -1,19 +1,16 @@
 
 clientType = 's';   // 客户端类型
 
-// 本地存储
-store.get(clientType) || store.set(clientType, {
-    talkID: ''
+$.get('/do/s/me', function(resObj) {
+    var lectid = resObj['lectid'];
+    if (lectid) { // 已有讲座
+        hashPage = location.hash || '#/view';
+    } else {
+        hashPage = location.hash || '#/open';
+    }
+    // 加载子页面
+    loadFrame(hashPage);
 });
-var storeS = store.get(clientType);
-
-if (storeS['talkID']) { // 已有讲座
-    hashPage = location.hash || '#/view';
-} else {
-    hashPage = location.hash || '#/open';
-}
-// 加载子页面
-loadFrame(hashPage);
 
 
 
