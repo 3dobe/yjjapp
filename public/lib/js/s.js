ef16681310fcb851635700fe2bcaea5808b871b6
node200ok <node200ok@163.com>
2013-11-01 12:37:42 +0800

    enter/open

--- public/lib/js/s.js
+++ public/lib/js/s.js
@@ -6,7 +6,7 @@ $.get('/do/s/me', function(resObj) {
     if (lectid) { // 已有讲座
         hashPage = location.hash || '#/view';
     } else {
-        hashPage = location.hash || '#/open';
+        hashPage = location.hash || '#/enter';
     }
     // 加载子页面
     loadFrame(hashPage);
