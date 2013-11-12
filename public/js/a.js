18922a8f5d52333fcbf4c5847a0c079011274069
node200ok <node200ok@163.com>
2013-11-13 00:23:12 +0800

    username

--- public/js/a.js
+++ public/js/a.js
@@ -1,21 +1,24 @@
 
 clientType = 'a';   // 客户端类型
 
+
 $.get('/do/a/me', function(resObj) {
-    var lectid = resObj['lectid'];
-    if (lectid) { // 已有讲座
-        hashPage = location.hash || '#/view';
-        $navbar.find('#sec-in').removeClass('hidden');
-    } else {
-        hashPage = location.hash || '#/join';
-        $navbar.find('#sec-out').removeClass('hidden');
-    }
-    // 加载子页面
-    loadFrame(hashPage);
+	var lectid = resObj['lectid'];
+	if (lectid) { // 已有讲座
+		hashPage = location.hash || '#/view';
+		$navbar.find('.sec').addClass('hidden')
+			.filter('#sec-in').removeClass('hidden');
+	} else {
+		hashPage = location.hash || '#/join';
+		$navbar.find('.sec').addClass('hidden')
+			.filter('#sec-out').removeClass('hidden');
+	}
+	// 加载子页面
+	loadFrame(hashPage);
 });
 
 heartbeat();
 setInterval(heartbeat, 1 * 60 * 1000);  // 1 min
 function heartbeat() {
-    $.get('/do/a/heartbeat');
+	$.get('/do/a/heartbeat');
 }
--- public/js/s.js
+++ public/js/s.js
@@ -1,15 +1,17 @@
 
 clientType = 's';   // 客户端类型
 
-$.get('/do/s/me', function(resObj) {
-    var lectid = resObj['lectid'];
-    if (lectid) { // 已有讲座
-        hashPage = location.hash || '#/view';
-        $navbar.find('#sec-in').removeClass('hidden');
-    } else {
-        hashPage = location.hash || '#/enter';
-        $navbar.find('#sec-out').removeClass('hidden');
-    }
-    // 加载子页面
-    loadFrame(hashPage);
+$.get('/do/s/me', function(resObj){
+	var lectid = resObj['lectid'];
+	if (lectid) { // 已有讲座
+		hashPage = location.hash || '#/view';
+		$navbar.find('.sec').addClass('hidden')
+			.filter('#sec-in').removeClass('hidden');
+	} else {
+		hashPage = location.hash || '#/enter';
+		$navbar.find('.sec').addClass('hidden')
+			.filter('#sec-out').removeClass('hidden');
+	}
+	// 加载子页面
+	loadFrame(hashPage);
 });
