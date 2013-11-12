f03d5ae9b2f446f2afae3536c4b0c9bc0121df75
node200ok <node200ok@163.com>
2013-11-12 15:53:37 +0800

    听端导航

--- public/lib/js/a.js
+++ public/lib/js/a.js
@@ -5,8 +5,10 @@ $.get('/do/a/me', function(resObj) {
     var lectid = resObj['lectid'];
     if (lectid) { // 已有讲座
         hashPage = location.hash || '#/view';
+        $navbar.find('#sec-in').removeClass('hidden');
     } else {
         hashPage = location.hash || '#/join';
+        $navbar.find('#sec-out').removeClass('hidden');
     }
     // 加载子页面
     loadFrame(hashPage);
