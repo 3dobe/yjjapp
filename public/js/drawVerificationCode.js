416a9458127bb04e5ad70abac969a3c1db8e98c2
funkyHui <540886643@qq.com>
2013-11-13 21:17:59 +0800

    modify

--- public/js/drawVerificationCode.js
+++ public/js/drawVerificationCode.js
@@ -2,9 +2,7 @@ $.fn.drawVerificationCode = function(firstArg, secondArg, operator){
     var ctx;
     ctx = this[0].getContext('2d');
     ctx.font = "15px Arial";
-
-    ctx.background = "rgb(0.255.255)";
     ctx.clearRect(0, 0, 70, 18);
-    ctx.fillText(firstArg + operator + secondArg + "＝?" ,7,15);
+    ctx.fillText(firstArg + operator + secondArg + "＝?" , 7, 15);
 
 }
