2a88283a7c628640235a56ecf3d7076e4b797e86
node200ok <node200ok@163.com>
2013-11-15 12:55:07 +0800

    aaa

--- public/js/takeoutAwardsList.js
+++ public/js/takeoutAwardsList.js
@@ -1,39 +1,40 @@
 $.fn.takeoutAwardsList = function(audilist, awlist, callback){
-    var num = awlist.length,
-        lihtml = '',
-        dtime = 70,
-        times = 50,
-        i = 0,
-        $self = $(this);
+	var num = awlist.length,
+		lihtml = '',
+		dtime = 70,
+		times = 50,
+		i = 0,
+		$self = $(this);
 
-    takeout();
-    function takeout(){
-        if(i==times){
-            for(var j=0;j<num;j++){
-                lihtml += '<li class="list-group-item">' + awlist[j].name + '</li>';
-            }
-            $self.html(lihtml);
-            lihtml = '';
-            return callback();
+	takeout();
+	function takeout(){
+		if(i==times){
+            ulhtml = '';
+			for(var j=0;j<num;j++){
+				ulhtml += '<li class="list-group-item">' + awlist[j].name + '</li>';
+			}
+			$self.html(ulhtml);
+			return callback && callback();
 
-        }else{
-            for(var j=0;j<num;j++){
-               lihtml += '<li class="list-group-item">' + _.sample(audilist).name +'</li>';
-            }
-            $self.html(lihtml);
-            lihtml = '';
-            i++;
-            setTimeout(takeout, dtime);
-        }
-    }
+		}else{
+			var arr = _.sample(audilist, num);
+            ulhtml = '';
+			for(var j=0;j<num;j++){
+			   ulhtml += '<li class="list-group-item">' + arr[j].name +'</li>';
+			}
+			$self.html(ulhtml);
+			i++;
+			setTimeout(takeout, dtime);
+		}
+	}
 }
 
 $.fn.setAwardsList = function(awlist){
-    var num = awlist.length,
-        lihtml = '';
-    for(var i=0;i<num;i++){
-        lihtml += '<li class="list-group-item">' + awlist[i].name +'</li>'
-    }
-    $(this).html(lihtml);
-    lihtml = '';
+	var num = awlist.length,
+		lihtml = '';
+	for(var i=0;i<num;i++){
+		lihtml += '<li class="list-group-item">' + awlist[i].name +'</li>'
+	}
+	$(this).html(lihtml);
+	lihtml = '';
 }
