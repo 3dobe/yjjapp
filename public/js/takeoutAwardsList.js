39f75ec45fb38f25f98f2019a2cb179668780538
funkyHui <540886643@qq.com>
2013-11-15 19:11:53 +0800

    color

--- public/js/takeoutAwardsList.js
+++ public/js/takeoutAwardsList.js
@@ -1,9 +1,10 @@
 $.fn.takeoutAwardsList = function(audilist, awlist, callback){
 	var num = awlist.length,
-		lihtml = '',
+		ulhtml = '',
 		dtime = 70,
 		times = 50,
 		i = 0,
+        colors = ['#00CC00','#0099FF','#3333FF','#6600CC','#66FFFF','#9933CC','#CCFF00','#FF3399','#FFFF00'],
 		$self = $(this);
 
 	takeout();
@@ -17,10 +18,10 @@ $.fn.takeoutAwardsList = function(audilist, awlist, callback){
 			return callback && callback();
 
 		}else{
-			var arr = _.sample(audilist, num);
+			var arr = _.sample(audilist, num),
             ulhtml = '';
 			for(var j=0;j<num;j++){
-			   ulhtml += '<li class="list-group-item">' + arr[j].name +'</li>';
+			   ulhtml += '<li class="list-group-item" style="color:' + _.sample(colors) +  ';">' + arr[j].name +'</li>';
 			}
 			$self.html(ulhtml);
 			i++;
@@ -31,10 +32,10 @@ $.fn.takeoutAwardsList = function(audilist, awlist, callback){
 
 $.fn.setAwardsList = function(awlist){
 	var num = awlist.length,
-		lihtml = '';
+		ulhtml = '';
 	for(var i=0;i<num;i++){
-		lihtml += '<li class="list-group-item">' + awlist[i].name +'</li>'
+		ulhtml += '<li class="list-group-item">' + awlist[i].name +'</li>'
 	}
-	$(this).html(lihtml);
-	lihtml = '';
+	$(this).html(ulhtml);
+	ulhtml = '';
 }
