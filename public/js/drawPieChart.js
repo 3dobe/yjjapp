eb1dd9dcd86f57a662e2a36bd14bb67ee438448f
Fritz-Lium <fritzlium@gmail.com>
2013-11-30 01:29:08 +0800

    word-break all

--- public/js/drawPieChart.js
+++ public/js/drawPieChart.js
@@ -16,6 +16,16 @@ $.fn.drawPieChart = function(options, colors){
             return option.count;
         });
     }
+
+    // fix chrome bug
+    if (_.isEqual(_.compact(values), [1])) {
+        var index = values.indexOf(1),
+            ix = index === 0 ? 1 : index - 1,
+            diff = 0.000001;
+        values[index] -= diff;
+        values[ix] += diff;
+    }
+
     _.each(values, function(val, i) {
         data.push({
             value: values[i],
@@ -24,5 +34,8 @@ $.fn.drawPieChart = function(options, colors){
     });
 
     ctx = this[0].getContext("2d");
-    myNewChart = new Chart(ctx).Pie(data);
+    myNewChart = new Chart(ctx).Pie(data, {
+        segmentStrokeWidth: 6,
+        animateScale: true
+    });
 }
