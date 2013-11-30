e3180232fff488fde6531938ebe72f934e1aee14
Fritz-Lium <fritzlium@gmail.com>
2013-11-30 17:27:04 +0800

    optimize

--- public/js/drawPieChart.js
+++ public/js/drawPieChart.js
@@ -1,41 +1,41 @@
 
 $.fn.drawPieChart = function(options, colors){
-    var ctx,
-        myNewChart,
-        values=[],
-        data = [];
+	var ctx,
+		myNewChart,
+		values=[],
+		data = [];
 
-    if (_.every(options, function(option) {
-        return option.count === 0;
-    })) {
-        values = _.map(options, function(option, i){
-            return 1;
-        });
-    } else {
-        values = _.map(options, function(option, i){
-            return option.count;
-        });
-    }
+	if (_.every(options, function(option) {
+		return option.count === 0;
+	})) {
+		values = _.map(options, function(option, i){
+			return 1;
+		});
+	} else {
+		values = _.map(options, function(option, i){
+			return option.count;
+		});
+	}
 
-    // fix chrome bug
-    if (_.isEqual(_.compact(values), [1])) {
-        var index = values.indexOf(1),
-            ix = index === 0 ? 1 : index - 1,
-            diff = 0.0000001;
-        values[index] -= diff;
-        values[ix] += diff;
-    }
+	// fix chrome bug
+	var index = values.indexOf(1);
+	if (index > -1 && _.compact(values).length < 2) {
+		var ix = index === 0 ? 1 : index - 1,
+			diff = 0.0000001;
+		values[index] -= diff;
+		values[ix] += diff;
+	}
 
-    _.each(values, function(val, i) {
-        data.push({
-            value: values[i],
-            color: colors[i]
-        });
-    });
+	data = _.map(values, function(val, i){
+		return {
+			color: colors[i],
+			value: val
+		}
+	});
 
-    ctx = this[0].getContext("2d");
-    myNewChart = new Chart(ctx).Pie(data, {
-        segmentStrokeWidth: 6,
-        animateScale: true
-    });
+	ctx = this[0].getContext("2d");
+	myNewChart = new Chart(ctx).Pie(data, {
+		segmentStrokeWidth: 6,
+		animateScale: true
+	});
 }
